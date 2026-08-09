d96b2b24ecb2b3934a870e4f5e2e2d83adf1ba58
MaxChenko <mchenko9@gmail.com>
2026-08-09 14:18:59 -0500

    added keymaps for live_grep and find_files

--- init.lua
+++ init.lua
@@ -990,3 +990,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
   callback = function() vim.lsp.buf.format { async = false } end,
 })
+
+-- Special keymaps
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Grep in project' })
